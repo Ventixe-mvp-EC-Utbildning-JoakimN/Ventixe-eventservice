b6593bafd5095a50fb56e2d15b2295bf8ba3614e
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-05-22 11:12:41 +0200

    localdb setup with seeded data

--- Ventixe-EventService/Program.cs
+++ Ventixe-EventService/Program.cs
@@ -1,23 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Ventixe_EventService.Data;
 using Ventixe_EventService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddSingleton<EventService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddDbContext<EventDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<EventService>();
+builder.Services.AddScoped<EventService>();
+
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<EventDbContext>();
+    SeedData.Initialize(context);
+}
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
